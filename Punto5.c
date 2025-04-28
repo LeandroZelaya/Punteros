63e6dfa5ef25ca5acbc3d5d4de162393f8ea7b55
LeandroZelaya <166446150+LeandroZelaya@users.noreply.github.com>
2025-04-28 20:25:38 -0300

    Add files via upload

--- Punto5.c
+++ Punto5.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <string.h>
-int i;
 
-concatenar(int *str, int *str2){
-	
+char *concatenar(char *ptr[], char *ptr2[]){
+	strcat(ptr, ptr2);
+	return ptr;
 }
 
 
 void main(){
 	char str[100];
 	char str2[100];
-	
+
 	printf("ingresar la cadena: ");
 	fgets(str,sizeof(str),stdin);
 	str[strcspn(str, "\n")] = '\0';
@@ -19,5 +19,5 @@ void main(){
 	fgets(str2,sizeof(str2),stdin);
 	str[strcspn(str2, "\n")] = '\0';
 	
-	concatenar(str, str2);
+	printf("%s",concatenar(str, str2));
 }
